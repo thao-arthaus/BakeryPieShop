d27c9b9ecf469c8943ffdfe9c5204be2cf3e1507
Thao Truong <Thao.Truong@arthaus.co.uk>
2019-10-28 11:54:15 +0000

    Add project files.

---
+++ BakeryPieShop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BakeryPieShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
