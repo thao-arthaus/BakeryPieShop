861399bfb06f1c24cab3944a91fca2263c734304
Thao Thi Ngoc Truong <ttngocthao_87@yahoo.com>
2019-10-29 22:43:15 +0000

    EF connected

--- BakeryPieShop/Startup.cs
+++ BakeryPieShop/Startup.cs
@@ -6,6 +6,7 @@ using BakeryPieShop.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,8 +25,11 @@ namespace BakeryPieShop
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<IPieRepository, MockPieData>();
-            services.AddScoped<ICategoryRepository, MockCategoryData>();
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            /*  services.AddScoped<IPieRepository, MockPieData>();
+              services.AddScoped<ICategoryRepository, MockCategoryData>();*/
+            services.AddScoped<IPieRepository, PieRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddControllersWithViews();
         }
 
