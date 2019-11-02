eb50cf0207c2aab7726d137c61297b8467f6e9f8
Thao Thi Ngoc Truong <ttngocthao_87@yahoo.com>
2019-11-02 11:57:24 +0000

    navigation to pie details works

--- BakeryPieShop/Controllers/PieController.cs
+++ BakeryPieShop/Controllers/PieController.cs
@@ -32,5 +32,20 @@ namespace BakeryPieShop.Controllers
             return View(pieListViewModel);
         }
 
+        public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieById(id);
+            if(pie == null)
+            {
+
+                return NotFound();
+            }
+            else
+            {
+                return View(pie);
+            }
+            
+        }
+
     }
 }
